bd9532d768370c2071f93b45a0f42804105586b0
roopagowda <roopagowda2294@gmail.com>
2016-11-10 10:15:46 +0530

    added helloworld1.java

---
+++ helloworld1.java
@@ -0,0 +1,10 @@
+public class helloworld1
+{
+// main method begins execution of Java application
+public static void main( String[] args )
+{
+System.out.println("Hello World!");
+System.out.println("Welcome to Java Programming! on Ubuntu 10.04");
+} // method main ends here
+} // end of the class Welcome
+
